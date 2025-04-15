fd5a4c011a1c2027ac68f4ac2547d6564582af68
Nifyy <nifsam@outlook.com>
2025-04-15 18:05:14 -0400

    Work week smth

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,13 +1,33 @@
-
-enum Spin {
-  up,
-  down
-}
-
+enum Spin { up, down }
 
 class Electron {
   int id;
-  static const num = 9.189e-28;
-  spin _spin;
+  static const num mass = 9.189e-28;
+  Spin spin;
+
+  Electron(this.id, this.spin);
 }
-//I made a change 
+
+class Person {
+  String name = '';
+  int age = 0;
+
+  void displayInfo() {
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+void main() {
+  Person person1 = Person();     // Create an object
+  person1.name = 'John';         // Assign Name
+  person1.age = 30;              // Assign Age
+
+  person1.displayInfo();         // Call the function
+
+  // Example usage of Electron
+  Electron e1 = Electron(1, Spin.up);
+  print('Electron ID: ${e1.id}');
+  print('Electron Spin: ${e1.spin}');
+  print('Electron Mass: ${Electron.mass}');
+}
--- lib/dart_application_1.dart
+++ lib/dart_application_1.dart
@@ -1,3 +1,21 @@
 int calculate() {
   return 6 * 7;
 }
+
+class Person {
+  String name = '';
+  int age = 0;
+
+  void displayInfo() {
+    print('Name: $name');
+    print('Age: $age');
+  }
+}
+
+void main() {
+  Person person1 = Person();
+  person1.name = 'Alice';
+  person1.age = 25;
+
+  person1.displayInfo();
+}
